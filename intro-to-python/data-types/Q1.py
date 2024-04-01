b8eb3ef6ce2d9c37ca88680895d9a03de73718ac
BxsedSosa <donovanxabad@gmail.com>
2024-03-31 19:50:18 -0700

    Completed Q2

---
+++ intro-to-python/data-types/Q1.py
@@ -0,0 +1,29 @@
+'''
+1. Identify the data type or class for each of the following values:
+
+Values
+------
+'True'
+False
+(1, 2, 3)
+1.5
+[1, 2, 3]
+2
+range(5)
+{1, 2, 3}
+None
+{'foo': 'bar'}
+'''
+
+'''
+String     str
+Boolean    bool
+Tuples     tuple
+Float      float
+List       list
+Integer    int
+Range      range
+Set        set
+NoneType   NoneType
+Dictionary Dict
+'''
---
+++ intro-to-python/data-types/Q2.py
@@ -0,0 +1,22 @@
+'''
+2. Create a tuple called 'names'. For instance:L
+
+Name
+------
+Asta
+Butterscotch
+Pudding
+Neptune
+Darwin
+
+You can use that data or make up your own
+'''
+
+names = ('Asta', 
+        'Butterscotch', 
+        'Pudding', 
+        'Neptune', 
+        'Darwin',
+)
+
+print(names)
