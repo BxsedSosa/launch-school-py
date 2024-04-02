fc8954d3ce3cf028272d25eb7f2e1ec2d3dd489c
BxsedSosa <donovanxabad@gmail.com>
2024-04-01 17:43:16 -0700

    Completed Q7

---
+++ intro-to-python/basic-operations/Q7.py
@@ -0,0 +1,16 @@
+'''
+7. What will the following code do? Why?
+
+int('3.1415')
+'''
+
+'''
+The code given has the int function to coerce the string of '3.1415' since we are trying to coerce a string which is also a floating number. The function int will coerce it just to the whole number of just 3
+'''
+
+'''
+Answer:
+
+It raises a `ValueError` since the string value 3.1415 does not represent a valid integer
+
+'''
