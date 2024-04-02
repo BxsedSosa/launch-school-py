a7672de59b1bbcb5d5faff6d2d5f8bfc52a36408
BxsedSosa <donovanxabad@gmail.com>
2024-04-02 15:05:51 -0700

    Completed Q1

---
+++ intro-to-python/variables/Q1.py
@@ -0,0 +1,25 @@
+'''
+Classify the following optential non-constant variable names as idiomctic, non-idomatic, or illegal. For the no-idiomatic and illegal names, explain your choice
+
+Names
+-----------
+index
+CatName
+lazy_dog
+quick_Fox
+1stCharacter
+operand2
+BIG_NUMBER
+n
+'''
+
+'''
+idiomatic
+non-idiomatic | It is using PascalCase naming convention
+idiomatic
+non-idiomatic | the second word is capitalized
+illegal       | It starts with a number
+idiomatic
+non-idiomatic | It is using SCREAMING_SNAKE_CASE
+non-idiomatic | pi is not a ASCII character
+'''
