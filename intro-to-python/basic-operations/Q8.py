5157f9fc678f01fbf8ba20abbb704ff76e06335b
BxsedSosa <donovanxabad@gmail.com>
2024-04-01 17:46:20 -0700

    Completed Q8

---
+++ intro-to-python/basic-operations/Q8.py
@@ -0,0 +1,9 @@
+'''
+8. To what value does the following expression evaluate?
+
+'12' < '9'
+'''
+
+'''
+From my knowledge when using a comparison operation such as less than with 2 string operands that have numbers. Python will take the first character of each operand and coerce them into a integer. So this expression will actually return True because 1 is less than 9
+'''
