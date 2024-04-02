f84433e79a9d48fdc1ffae3afd01587fd44f0eb6
BxsedSosa <donovanxabad@gmail.com>
2024-04-02 15:08:29 -0700

    Completed Q2

---
+++ intro-to-python/variables/Q2.py
@@ -0,0 +1,18 @@
+'''
+Classify the following potential function variable names as idiomctic, non-idomatic, or illegal. For the no-idiomatic and illegal names, explain your choice
+
+Names
+-----------
+index
+CatName
+lazy_dog
+quick_Fox
+1stCharacter
+operand2
+BIG_NUMBER
+n
+'''
+
+'''
+Everything in Q1 are the same answers
+'''
