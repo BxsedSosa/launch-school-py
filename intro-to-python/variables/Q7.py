e7107e1e5c13649b79c3a7a07cab094f9bbc708c
BxsedSosa <donovanxabad@gmail.com>
2024-04-02 15:30:42 -0700

    Completed Q7

---
+++ intro-to-python/variables/Q7.py
@@ -0,0 +1,18 @@
+'''
+7, What happens when you run the following code? Why?
+
+NAME = 'Victor'
+print('Good Morning, ' + NAME)
+print('Good Afternoon, ' + NAME)
+print('Good Evening, ' + NAME)
+
+NAME = 'Nina'
+print('Good Morning, ' + NAME)
+print('Good Afternoon, ' + NAME)
+print('Good Evening, ' + NAME)
+
+'''
+
+'''
+In this code above it will all run properly but the issues lies with in the intent of what the NAME constant should be used like. Here we see the NAME constant gettign assigned then reassigned which yes techincally its allowed in python becase python doesn't have hard coded constants unlike other lanuages. But since the intent of it being a constant was intially was wanted then it should be kept that way.
+'''
