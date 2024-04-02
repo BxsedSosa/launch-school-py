fc6a46f4f80b944b3b5707c76b6fb222f9bc805a
BxsedSosa <donovanxabad@gmail.com>
2024-04-02 15:12:43 -0700

    Completed Q3

---
+++ intro-to-python/variables/Q3.py
@@ -0,0 +1,25 @@
+'''
+Classify the following optential constant names as idiomctic, non-idomatic, or illegal. For the no-idiomatic and illegal names, explain your choice
+
+Names
+-----------
+index
+CatName
+snake_case
+lazy_dog
+1ST
+operand2
+BIG_NUMBER
+n (pi)
+'''
+
+'''
+non-idiomatic  | not in all caps
+non-idiomatic  | Using PascalCase naming convention
+non-idiomatic  | using snake_case but not screaming
+legal
+illegal        | Starts with a digit
+non-idiomatic  | Not using SCREAMING_SNAKE_CASE
+legal
+non-idiomatic  | Uses extened ASCII characters
+'''
