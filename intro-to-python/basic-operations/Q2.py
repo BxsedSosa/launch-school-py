9da75c060749956378e6697e3c296f4943d77c42
BxsedSosa <donovanxabad@gmail.com>
2024-04-01 17:28:24 -0700

    Completed Q2

---
+++ intro-to-python/basic-operations/Q2.py
@@ -0,0 +1,12 @@
+'''
+2. This question may be a little challenging if your math skills are rusty. Don't be afraid to take advantage of the hints. Try your best to solve the problem, but don't feel compelled to complete it if you become frustrated.
+
+Use the REPL and the arithmetic operatiors to extract the inditival digits of 4936:
+
+    1. One place is 6
+    2. Tens place is 3
+    3. Hundreds place is 9
+    4. Thousands place is 4
+
+
+'''
