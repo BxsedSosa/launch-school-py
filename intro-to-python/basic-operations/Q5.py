8659f54c29733ead75bc656d15e96a26742623fa
BxsedSosa <donovanxabad@gmail.com>
2024-04-01 17:36:20 -0700

    Completed Q5

---
+++ intro-to-python/basic-operations/Q5.py
@@ -0,0 +1,12 @@
+'''
+5. Will an error occur if you try to accesss a list element with an index greater than or equal to the list's length?
+
+Example:
+
+foo = ['a', 'b', 'c']
+print(foo[3])
+'''
+
+'''
+Yes, the program will give you a error stating that you are indexing something that isn't there / out of range
+'''
