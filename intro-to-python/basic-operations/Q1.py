e4a5c87b0a7dce736202000f44477d13841a32ca
BxsedSosa <donovanxabad@gmail.com>
2024-04-01 17:16:44 -0700

    Completed Q1

---
+++ intro-to-python/basic-operations/Q1.py
@@ -0,0 +1,47 @@
+'''
+1. Concatenate two strings, one with your first name and one with your last, to create a new string with your full name as its value
+
+For example, if your name is John Doe, you should combine 'John' and 'Doe' to get 'John Doe'
+'''
+
+'''
+P:
+The question is asking us to take two seperate strings which will be our first and last name.
+
+Then to concatenate them together as one whole string
+
+
+
+
+E:
+
+  Input:
+    'John'
+    'Doe'
+
+  Output:
+    'John Doe'
+
+D:
+
+  Data:
+    str
+
+
+
+A:
+
+  Create a variable called first_name equal to the string 'Donovan'
+  Create a variable called last_name equal to the string 'Abad'
+
+  then we will use the print function inside the parentheses we will have first_name plus a empty string space plus last_name
+
+
+
+C:
+'''
+
+first_name = 'Donovan'
+last_name = 'Abad'
+
+print(first_name + ' ' + last_name)
