ef8d15cbe2d419f39298e8fe30c5e84b6bd5a235
BxsedSosa <donovanxabad@gmail.com>
2024-04-02 15:26:47 -0700

    Completed Q6

---
+++ intro-to-python/variables/Q6.py
@@ -0,0 +1,11 @@
+'''
+6. Write a program named age.py that includes someone's age an then calculates and report sthe future age 10, 20, 30, 40 years from now
+'''
+
+AGE = 25
+
+print(f'You are {AGE} years old')
+print(f'In 10 years, you will be {AGE + 10} years old')
+print(f'In 20 years, you will be {AGE + 20} years old')
+print(f'In 30 years, you will be {AGE + 30} years old')
+print(f'In 40 years, you will be {AGE + 40} years old')
