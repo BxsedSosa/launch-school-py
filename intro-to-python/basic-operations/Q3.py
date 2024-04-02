91c55c25283604c2d56985a1aac889d3e01ee56a
BxsedSosa <donovanxabad@gmail.com>
2024-04-01 17:32:03 -0700

    Completed Q3

---
+++ intro-to-python/basic-operations/Q3.py
@@ -0,0 +1,13 @@
+'''
+3. What does the following code do? Why?
+
+print('5' + '10')
+'''
+
+'''
+In this code the two operands in the print function are both strings so the output of the example will show us: 
+
+'510'
+
+The reason why this is the case is because both operands are strings and they concatenate together just as if the characters were letters instead. It may look confusing at first glance for new programmers since you'd automatically think that since you see two numbers you would just add them together.
+'''
