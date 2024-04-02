2210eaecd1a9ae189cd536faaa06e713fe635194
BxsedSosa <donovanxabad@gmail.com>
2024-04-01 17:38:23 -0700

    Completed Q6

---
+++ intro-to-python/basic-operations/Q6.py
@@ -0,0 +1,9 @@
+'''
+6. To what value does the following expression evaluete?
+
+'foo' == 'Foo'
+'''
+
+'''
+The following expresion will evaluate to a False because, they might say the same thing but Python takes everything literally so in the program they are diffrernt because the capitalization matters
+'''
