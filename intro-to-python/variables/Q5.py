5e5f8f9f2654d6d9ca4c4daf4acf6dfcb33b4ddd
BxsedSosa <donovanxabad@gmail.com>
2024-04-02 15:21:35 -0700

    Completed Q5

---
+++ intro-to-python/variables/Q5.py
@@ -0,0 +1,13 @@
+'''
+Write a program named greeter.py that greets 'Victor' three times. Your program should use a variable and not hard code the string value 'Victor' in each greeting. Here's an example
+
+Good Morning, Victor
+Good Afternoon, Victor
+Good Evening, Victor
+'''
+
+NAME = 'Victor'
+
+print('Good Morning ' + NAME)
+print('Good Afternoon ' + NAME)
+print('Good Evening ' + NAME)
