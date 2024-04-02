b494baf03a25e17af1b295464a04043073340c4c
BxsedSosa <donovanxabad@gmail.com>
2024-04-02 15:16:48 -0700

    Completed Q4

---
+++ intro-to-python/variables/Q4.py
@@ -0,0 +1,23 @@
+'''
+Classify the following optential class names as idiomctic, non-idomatic, or illegal. For the no-idiomatic and illegal names, explain your choice
+
+Names
+-----------
+index
+CatName
+lazy_dog
+1ST
+operand2
+BigNumber3
+n (pi)
+'''
+
+'''
+non-idiomatic | doesn't start with capitalizd letter
+legal
+non-idiomatic | uses underscore
+illegal       | uses digit
+non-idiomatic | doesn'tstart with capitalized letter
+legal
+non-idiomatic | uses exted ASCII character
+'''
