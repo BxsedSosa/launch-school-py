adcc8638307b8085e4de037ebb271bceab48198e
BxsedSosa <donovanxabad@gmail.com>
2024-04-03 13:55:01 -0700

    Completed Q7

---
+++ intro-to-python/function-methods/Q7.py
@@ -0,0 +1,13 @@
+'''
+7. Without running the following code, what do you think it will do?
+'''
+
+def foo(bar, qux):
+    print(bar)
+    print(qux)
+
+foo('Hello')
+
+'''
+I believe that the program will run. will give an error due to the function invokation only having 1 argument when it requires to fill in 2.
+'''
