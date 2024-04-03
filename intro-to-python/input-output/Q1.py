53aab846a744f41e2ef34cf2031f3cbf808359d3
BxsedSosa <donovanxabad@gmail.com>
2024-04-02 18:30:16 -0700

    Completed Q1

---
+++ intro-to-python/input-output/Q1.py
@@ -0,0 +1,7 @@
+'''
+1. Write a program named greeter.py the program should ask for your name, then output 'Hello, NAME!' where NAME is the name you entered
+'''
+
+name = str(input('What is your name? \n'))
+
+print(f'Hello, {name}!')
