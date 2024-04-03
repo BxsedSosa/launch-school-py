ae83db8118cedb6346bc0c8040c18a642aca3f70
BxsedSosa <donovanxabad@gmail.com>
2024-04-03 14:06:45 -0700

    Completed Q12

---
+++ intro-to-python/function-methods/Q12.py
@@ -0,0 +1,14 @@
+'''
+12. Without running the following code, what do you think it will do?
+'''
+
+def foo(first, second=3, third=2):
+    print(first)
+    print(second)
+    print(third)
+
+foo()
+
+'''
+This code will give an error due to the parameter not recieveing a arugment because there is not a default parameter set in place
+'''
