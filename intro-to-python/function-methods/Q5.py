28f701f1b434f2a7e6e68c73ecd8f425e16c7385
BxsedSosa <donovanxabad@gmail.com>
2024-04-03 13:48:53 -0700

    Completed Q5.py

---
+++ intro-to-python/function-methods/Q5.py
@@ -0,0 +1,12 @@
+'''
+5. What does the following code print?
+'''
+
+def scream(words):
+    return words + '!!!!'
+
+scream('Yipeee')
+
+'''
+The wont print out anything since the function is only reutrning the value back and there is no print function being used around teh scream invocation
+'''
