e610d10bfc658ddf703ed5b54d3eddd72d255820
BxsedSosa <donovanxabad@gmail.com>
2024-04-03 13:58:19 -0700

    Completed Q8

---
+++ intro-to-python/function-methods/Q8.py
@@ -0,0 +1,13 @@
+'''
+8. Without running the following code, what do you think it will do?
+'''
+
+def foo(bar, qux):
+    print(bar)
+    print(qux)
+
+foo(42, 3.141592, 2.718)
+
+'''
+I believe that the code will give an error due to there being too many arguemnts compared to the amount of parameters
+'''
