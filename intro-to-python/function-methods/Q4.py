6ad7d75be64a79d8d9362716115738608e7f652c
BxsedSosa <donovanxabad@gmail.com>
2024-04-03 13:46:34 -0700

    Completed Q4

---
+++ intro-to-python/function-methods/Q4.py
@@ -0,0 +1,54 @@
+'''
+4. Consider the code
+
+Identify the following items in the code:
+
+ITEM
+-------------------------
+function name
+function arguments
+function definition
+function body
+function parameters
+function invocation
+function return value
+all identifiers
+'''
+
+def multiply_numbers(num1, num2, num3):
+    result = num1 * num2 * num3
+    return result
+
+product = multiply_numbers(2, 3, 4)
+
+'''
+Function name = 
+    multiply_numebrs
+
+function arguments = 
+    2, 3, 4
+
+function definition = 
+    everything on lines 1-3
+
+function body = 
+    result = num1 * num2 * num3
+    return result
+
+function parameters = 
+    num1, num2, num3
+
+function invocation = 
+    multiply_numers(2, 3, 4)
+    
+function return value =
+    result
+    
+all identifiers = 
+    multiply_numbers
+    num1
+    num2
+    num3
+    result
+    product
+'''
