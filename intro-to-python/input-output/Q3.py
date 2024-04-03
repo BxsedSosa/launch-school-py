b287091bddd38c6d12405642cb95cd6116fe1026
BxsedSosa <donovanxabad@gmail.com>
2024-04-02 18:35:40 -0700

    Completed Q3

---
+++ intro-to-python/input-output/Q3.py
@@ -0,0 +1,11 @@
+'''
+3. Write a program that asks the user to eneter their age, then calculates the reports the future age 10, 20, 30, 40.
+'''
+
+AGE = int(input('What is your age?: \n'))
+
+print(f'You are {AGE} years old')
+print(f'In 10 years, you will be {AGE + 10} years old.')
+print(f'In 20 years, you will be {AGE + 20} years old.')
+print(f'In 30 years, you will be {AGE + 30} years old.')
+print(f'In 40 years, you will be {AGE + 40} years old.')
