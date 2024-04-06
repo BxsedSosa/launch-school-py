84557297aa0036e9c7687c1ab80ffd8a1110b27b
BxsedSosa <donovanxabad@gmail.com>
2024-04-05 22:41:14 -0700

    Completed Q1

---
+++ intro-to-python/intro-collections/Q1.py
@@ -0,0 +1,9 @@
+'''
+1. If you have a list named people, how can you determin the number of entries in that list?
+'''
+
+'''
+You would use the len function to check how many elements are within that list
+
+len(people)
+'''
