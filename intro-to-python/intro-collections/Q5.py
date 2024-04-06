5cd5bc41fe43eb2c4a8d3538b3131655e132be47
BxsedSosa <donovanxabad@gmail.com>
2024-04-05 22:59:22 -0700

    Completed Q5

---
+++ intro-to-python/intro-collections/Q5.py
@@ -0,0 +1,13 @@
+'''
+5. How do the set types differ from teh sequence types?
+'''
+
+'''
+Sets and sequence types differ from
+
+Sets only allow unique objects within its collection so there will not be any duplicates
+
+Sets are also unordered unlike sequences
+
+Sets do not support indexing
+'''
