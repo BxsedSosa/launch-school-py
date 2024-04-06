e335f39bbdb3b7b8b55ef04dd8e3e6465054baf7
BxsedSosa <donovanxabad@gmail.com>
2024-04-05 22:49:12 -0700

    Completed Q3

---
+++ intro-to-python/intro-collections/Q3.py
@@ -0,0 +1,13 @@
+'''
+3. identify at least 2 differences between lists and tuples. Identify at least 2 ways that list and tuples are similar
+'''
+
+'''
+Differences:
+One is mutable and the other is immutable
+You start and end a list with [] and tuples is ()
+
+Similarity:
+Both are under the sequences category
+and both take Non-primitive
+'''
