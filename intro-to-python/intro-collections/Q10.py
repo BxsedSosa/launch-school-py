644fbbf7f48517c473329a190a2052f08af3b199
BxsedSosa <donovanxabad@gmail.com>
2024-04-06 11:36:34 -0700

    Completed Q10

---
+++ intro-to-python/intro-collections/Q10.py
@@ -0,0 +1,9 @@
+'''
+10. Bob expects the following code to print the names in alphabetical order without running the code, cana you say wheather it will? Explain your answer
+
+names = {'Chris', 'Clare', 'Karis', 'Karl', 'Max', 'Nick', 'Viscotr}
+print(names)
+'''
+'''
+This collection will not be able to organize the names by alphbetical order because it is a set type they are using. Which are a unordered collection of data so sets never look for itself to be in order.
+'''
