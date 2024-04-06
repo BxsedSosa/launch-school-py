4bb7cd0786e0ad5988185b1f32a45eb3074aeaa8
BxsedSosa <donovanxabad@gmail.com>
2024-04-05 22:55:39 -0700

    Completed Q4

---
+++ intro-to-python/intro-collections/Q4.py
@@ -0,0 +1,10 @@
+'''
+4. Why can we treat strings as sequences?
+'''
+
+'''
+Strings are a form of squenence called text sequence.
+
+Strings are homogenous; all characters in a string are characters
+Strings are ordered and you can access them individually by charcters through indexing
+'''
