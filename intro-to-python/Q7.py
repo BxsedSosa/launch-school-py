4f19783c16f56c80782c6d6f95b56e76e6864187
BxsedSosa <donovanxabad@gmail.com>
2024-04-06 11:08:12 -0700

    Completed Q7

---
+++ intro-to-python/Q7.py
@@ -0,0 +1,18 @@
+'''
+7. Without running the following code, identify the numbers that are included in each of the following ranges:
+
+range(7)
+range(1, 6)
+range(3, 15, 4)
+range(3, 8, -1)
+range(8, 3, -1)
+'''
+
+'''
+The code will return per each range function used:
+
+0 - 6
+1 - 5
+3, 7, 11
+8, 7, 6, 5, 4
+'''
