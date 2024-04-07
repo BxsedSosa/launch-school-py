9003e5cb7e35492f8978df2537344510c6b4f80b
BxsedSosa <donovanxabad@gmail.com>
2024-04-07 14:07:19 -0700

    Completed Q13

---
+++ intro-to-python/using-collections/Q13.py
@@ -0,0 +1,16 @@
+'''
+13. Without running the following code, determin what each print statement should print.
+'''
+
+cats = ('Cocoa', 'Cheddar', 'Pudding', 'Butterscotch')
+print('Butterscotch' in cats)
+print('Butter' in cats)
+print('Butter' in cats[3])
+print('cheddar' in cats)
+
+'''
+True
+False
+True
+False
+'''
