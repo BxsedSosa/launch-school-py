5ef0970f6dc57bc7bcca1479809115dc9282c215
BxsedSosa <donovanxabad@gmail.com>
2024-04-07 12:36:10 -0700

    Completed Q2.py ;

---
+++ intro-to-python/using-collections/Q2.py
@@ -0,0 +1,7 @@
+'''
+2. Use slicing to write a Python code to print a 6-Char substrin go 'Launch School' that begins with the first c
+'''
+
+string = 'Launch School'
+new_string = string[4:10]
+print(new_string)
