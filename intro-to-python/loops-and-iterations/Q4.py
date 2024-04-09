1b50e20fbde7c212d12ab345ebcf7f551aa4ca5a
BxsedSosa <donovanxabad@gmail.com>
2024-04-09 04:56:34 -0700

    Completed Q4

--- intro-to-python/loops-and-iterations/Q4.py
+++ intro-to-python/loops-and-iterations/Q4.py
@@ -1,18 +1,19 @@
-'''
+"""
 4. Use a while loop to print all numbers in my_list with even values, one number per line. Then print, the odd numbers using a 'for' loop
-'''
+"""
+
 my_list = [6, 3, 0, 11, 20, 4, 17]
 counter = 0
 
-print('Even numbers: \n --------------------')
+print("Even numbers: \n --------------------")
 while counter < len(my_list):
-    if counter % 2 == 0:
+    if my_list[counter] % 2 == 0:
         print(my_list[counter])
 
     counter += 1
 
 
-print('Odd numbers: \n --------------------')
+print("Odd numbers: \n --------------------")
 for i in my_list:
     if i % 2 == 1:
         print(i)
